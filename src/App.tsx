f58b88fe9d0eb31dca62ebd71ddaba0e8658cd10
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-01-20 20:59:03 +0000

    Add shopping list component with input and item display

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [itemToAdd, setItemToAdd] = useState("");
+  const [shoppingList, setShoppingList] = useState<string[]>([]);
+
+  function addListItem(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter" && itemToAdd.trim() !== "") {
+      setShoppingList((prev) => [...prev, itemToAdd]);
+      setItemToAdd("");
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Shopping List</h1>
+        <input
+          type="text"
+          value={itemToAdd}
+          onChange={(e) => setItemToAdd(e.target.value)}
+          onKeyDown={(e) => addListItem(e)}
+        />
+
+        <ul>
+          {shoppingList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
