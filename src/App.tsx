727cf41e68919a74d289be955ad5d11ac41b569e
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-01-20 21:33:10 +0000

    Add checkbox to toggle completed state

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@ import { useState } from "react";
 
 function App() {
   const [itemToAdd, setItemToAdd] = useState("");
-  const [shoppingList, setShoppingList] = useState<string[]>([]);
+  const [shoppingList, setShoppingList] = useState<
+    { text: string; completed: boolean }[]
+  >([]);
 
   function addListItem(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === "Enter" && itemToAdd.trim() !== "") {
-      setShoppingList((prev) => [...prev, itemToAdd]);
+      setShoppingList((prev) => [
+        ...prev,
+        {
+          text: itemToAdd,
+          completed: false,
+        },
+      ]);
       setItemToAdd("");
     }
+  }
+
+  function toggleItem(indexToToggle: number) {
+    setShoppingList((prev) =>
+      prev.map((item, index) =>
+        index === indexToToggle
+          ? { ...item, completed: !item.completed }
+          : item,
+      ),
+    );
   }
 
   return (
@@ -24,7 +42,16 @@ function App() {
 
         <ul>
           {shoppingList.map((item, index) => (
-            <li key={index}>{item}</li>
+            <li key={index}>
+              <input
+                type="checkbox"
+                checked={item.completed}
+                onChange={() => toggleItem(index)}
+              />
+              <span className={item.completed ? "completed" : ""}>
+                {item.text}
+              </span>
+            </li>
           ))}
         </ul>
       </div>
