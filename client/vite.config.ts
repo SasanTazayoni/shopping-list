7bb78a62406d97579df83df478e1899fcc6d7411
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-05-10 14:19:47 +0100

    Remove incorrect base path from vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shopping-list/',
-  server: {
+server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
