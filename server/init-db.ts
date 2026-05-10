151ebdbd68c06c2a08d01af8f0b9051be7ac719c
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-05-10 14:00:52 +0100

    Correct pool import in init-db.ts

--- server/init-db.ts
+++ server/init-db.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { readFileSync } from "fs";
-import pool from "./db.js";
+import { pool } from "./db.js";
 
 const sql = readFileSync(new URL("./schema.sql", import.meta.url), "utf-8");
 await pool.query(sql);
